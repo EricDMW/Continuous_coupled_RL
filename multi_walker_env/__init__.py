fe7845b335f08671cb6e5b41d79fb1e5514017ab
EricDMW <wdong025@ucr.edu>
2025-03-02 21:13:05 -0800

    initial commit

---
+++ multi_walker_env/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@File   : __init__.py
+@Author : Dongming Wang
+@Email  : dongming.wang@email.ucr.edu
+@Project: $(basename ~/project/continuous_coupled_rl/multi_walker_env)
+@Date   : 03/02/2025
+@Time   : 21:12:47
+@Info   : Description of the script
+"""
+
+
+def main():
+   #BUILD THE MAIN FUNCTON
+
+if __name__ == '__main__':
+    main()
